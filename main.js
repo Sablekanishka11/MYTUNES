56b4921291cebd2e25c3084192f709bb1ca6ff01
Sablekanishka11 <103958844+Sablekanishka11@users.noreply.github.com>
2022-04-19 00:06:15 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,62 @@
+main.js
+Who has access
+Not shared
+System properties
+Type
+JavaScript
+Size
+810 bytes
+Storage used
+810 bytes
+Location
+MYTUNES
+Owner
+me
+Modified
+00:00 by me
+Opened
+00:05 by me
+Created
+00:01 with Google Drive
+Add a description
+Viewers can download
+//Responsive nav
+$(function() {
+	menu = $('nav ul');
+
+  $('#openup').on('click', function(e) {
+    e.preventDefault(); menu.slideToggle();
+  });
+
+  $(window).resize(function(){
+    var w = $(this).width(); if(w > 480 && menu.is(':hidden')) {
+      menu.removeAttr('style');
+    }
+  });
+
+  $('nav li').on('click', function(e) {
+    var w = $(window).width(); if(w < 480 ) {
+      menu.slideToggle();
+    }
+  });
+  $('.open-menu').height($(window).height());
+});
+
+// Smooth Scrolling
+$('.cf a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
